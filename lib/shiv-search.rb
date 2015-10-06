3fc3d4bc7e72ac5920ceafd9d0210a0bb89658dd
Your Name <you@example.com>
2015-10-06 21:13:07 +0000

    + trait search

--- lib/shiv-search.rb
+++ lib/shiv-search.rb
@@ -1,15 +1,50 @@
 desc 'Search related tasks'
 
-command [:search, :searches] do |s|
+command [:search] do |s|
 
-  s.desc 'search'
-  s.arg_name 'search'
-  s.command [:search] do |search|
-  search.action do |global_options,options,args|
+  s.desc 'string'
+  s.arg_name 'all'
+  s.command [:all] do |search|
+    search.action do |global_options,options,args|
       help_now!('search string is required') if args.empty?
-      #host_id = ShivHost.get_host_id_from_hostname(args.shift)
       search_text = args[0]
       payload = { "search_text" => search_text, :auth_token => $token}.to_json
-      url = "#{$url}/search.json"
-      response = (RestClient.post url, payload, :content_type => :json)
+      url = "#{$url}/cli/searchAll.json"
+      response = JSON.parse((RestClient.post url, payload, :content_type => :json))
+      if $format == 'yaml'
+        puts response.to_yaml
+      elsif $format == 'json'
+        puts response.to_json
+      else
+        puts response.to_yaml
+      end
+    end
   end
+
+
+  s.desc 'var=val'
+  s.arg_name 'var=val'
+  s.command [:trait] do |trait_search|
+    trait_search.action do |global_options,options,args|
+
+      help_now!('search string is required') if args.empty?
+      search_text = args[0]
+      if ShivHost.validtrait?(search_text)
+        payload = { "search_text" => search_text, :auth_token => $token}.to_json
+        url = "#{$url}/cli/searchTrait.json"
+        response = JSON.parse((RestClient.post url, payload, :content_type => :json))
+        if $format == 'yaml'
+          puts response.to_yaml
+        elsif $format == 'json'
+          puts response.to_json
+        else
+          puts response.to_yaml
+        end
+      else
+        help_now!("Invalid trait found: #{search_text}   Valid trait: foo=bar")
+      end
+    end
+  end
+
+
+end
